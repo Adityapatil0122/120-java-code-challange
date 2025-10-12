40c82fefb873aca10122b27321fcd3d046f050f7
Adityapatil0122 <161105619+Adityapatil0122@users.noreply.github.com>
2025-10-12 10:33:38 +0530

    Add files via upload

---
+++ avgof3intigers.java
@@ -0,0 +1,12 @@
+// WAP to calculate average of three integer nos.
+class avgof3intigers {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 30;
+
+        int d = (a + b + c) / 3;
+
+        System.out.println("Average = "+d);
+    }
+}

d0d6250cb8a2f5177861ab9a74603d13addaaf71
Aditya Patil <161105619+Adityapatil0122@users.noreply.github.com>
2026-01-13 20:20:02 +0530

    Add files via upload

---
+++ Arrays2d.java
@@ -0,0 +1,32 @@
+/*
+public class Arrays2d {
+    public static void main(String[] args) {
+        String[][] cars = new String[3][3];
+
+        cars[0][0] = "Alto";
+        cars[0][1] = "Alto";
+        cars[0][2] = "Alto";
+        cars[1][0] = "Alto";
+        cars[1][1] = "Alto";
+        cars[1][2] = "Alto";
+        cars[2][0] = "Alto";
+        cars[2][1] = "Alto";
+        cars[2][2] = "Alto";
+
+        //or
+   // String [][] cars = { {"Alto","Alto","Alto"}
+     //                    {"Alto","Alto","Alto"}
+       //                  {"Alto","Alto","Alto"}
+         //               };
+
+        for (int i = 0; i < cars.length; i++) {
+            System.out.println();
+
+            for (int j = 0; j < cars[i].length; j++) {
+                System.out.print(" "+cars[i][j]);
+            }
+        }
+
+    }
+}
+*/

b2ba172724d23e1a99bcb260cd83d69e5e51b10c
Aditya Patil <161105619+Adityapatil0122@users.noreply.github.com>
2026-01-17 23:49:20 +0530

    Inheritance code practice

---
+++ Animal.java
@@ -0,0 +1,12 @@
+public class Animal {
+
+        String name = "Dog";
+        boolean isAlive;
+        int total = 2;
+        Animal(){
+                isAlive = true;
+        }
+        void eat() {
+            System.out.println("Yoo MF I SMOKE WEED");
+        }
+    }
